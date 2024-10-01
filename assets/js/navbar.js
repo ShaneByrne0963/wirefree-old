09a776d73e319eb75c6e9022126ca33daf3e0d06
Shane Byrne <shanebyrne0963@gmail.com>
2024-10-01 13:27:44 +0000

    Add ability to clear all elements from canvas

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,14 @@
-
+/**
+ * Clears the canvas of all elements
+ */
+function clearCanvas() {
+    $('#canvas').empty();
+}
 
 $(document).ready(() => {
+    $('#file-new-project').click(() => {
+        clearCanvas();
+    });
     $('#window-side-panel').click(() => {
         openElement('#side-panel');
     });
