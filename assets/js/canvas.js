eee454efe6f224947ebdc60087c22bd54bb51673
Shane Byrne <shanebyrne0963@gmail.com>
2024-10-01 14:22:14 +0000

    Implement shaped elements in canvas

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -17,9 +17,12 @@ function canvasMouseDown(e) {
  * @param {Object} e The event object created with the event
  */
 function canvasMouseUp(e) {
-    $('.canvas-element.sizing').removeClass('sizing');
+    $('.canvas-element.sizing').removeClass('sizing').removeAttr('data-origin-x').removeAttr('data-origin-y');
 }
 
+/**
+ * Changes the size of the canvas element that is currently being resized
+ */
 function sizeCanvasElement() {
     let sizingElement = $('.canvas-element.sizing').get(0);
     if (sizingElement) {
@@ -28,7 +31,39 @@ function sizeCanvasElement() {
         let yOrigin = $(sizingElement).attr('data-origin-y');
         let width = Math.abs(xOrigin - mouse.xCanvas);
         let height = Math.abs(yOrigin - mouse.yCanvas);
-        $(sizingElement).css('width', `${width}px`).css('height', `${height}px`);
+
+        //Triangles are sized differently to other shapes
+        if (mouse.selectedShape.includes('triangle')) {
+            let triangleType = mouse.selectedShape.slice(9);
+            switch (triangleType) {
+                case 'up':
+                    $(sizingElement).css('border-bottom', `${height}px solid red`)
+                        .css('border-left', `${width / 2}px solid transparent`)
+                        .css('border-right', `${width / 2}px solid transparent`);
+                    break;
+                case 'right':
+                    $(sizingElement).css('border-left', `${width}px solid red`)
+                        .css('border-top', `${height / 2}px solid transparent`)
+                        .css('border-bottom', `${height / 2}px solid transparent`);
+                    break;
+                case 'down':
+                    $(sizingElement).css('border-top', `${height}px solid red`)
+                        .css('border-left', `${width / 2}px solid transparent`)
+                        .css('border-right', `${width / 2}px solid transparent`);
+                    break;
+                case 'left':
+                    $(sizingElement).css('border-right', `${width}px solid red`)
+                        .css('border-top', `${height / 2}px solid transparent`)
+                        .css('border-bottom', `${height / 2}px solid transparent`);
+                    break;
+                default:
+                    break;
+            }
+        }
+        // All other shapes
+        else {
+            $(sizingElement).css('width', `${width}px`).css('height', `${height}px`);
+        }
 
         // Allowing negative sizes
         if (mouse.xCanvas < xOrigin) {
